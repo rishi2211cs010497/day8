fd90f91e23d4f1169ac0fa33bfd31bca38039d64
anil2211cs010470 <aniluu1818@gmail.com>
2025-01-20 21:42:33 +0530

    pulling

---
+++ array.js
@@ -0,0 +1,35 @@
+//let user1 = "Rishi";
+//let user2 = "Samyu";
+//let user3 = "Hanuu";
+//let user4 = "Valli";
+//let user5 = "Bhanuu";
+
+//console.log(user[1]);
+//console.log(user[2]);
+//console.log(user[3]);
+//console.log(user[4]);
+//console.log(user[5]);
+
+//let users = ["Rishi" , "Samyu" , "Valli" , "Hanuu" , "Bhanu"];
+
+//users.map((user, i)=>{
+  //  console.log(user, i);
+//})
+
+//let x =users.map((user ,i)=>{
+  //  return user;
+//});
+//console.log(x);
+
+//let x = users.forEach((user,i)=>{
+  //  console.log(user);
+    //return user;
+//});
+//console.log(x);
+
+//let userDetails = {
+  //  name :"Akshay",
+    //age:22,
+    //city:"Hyderabad"
+//};
+
